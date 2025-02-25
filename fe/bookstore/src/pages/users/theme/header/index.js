64a55f6e0c10e94817ffdc9deef44da0bb0689c8
Huynh Nam <namhuynh.101104@gmail.com>
2025-02-25 20:44:41 +0700

    Xong B7

--- fe/bookstore/src/pages/users/theme/header/index.js
+++ fe/bookstore/src/pages/users/theme/header/index.js
@@ -1,56 +1,137 @@
 import "./style.scss";
-import {AiTwotoneMail ,AiOutlineUser ,AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineTwitter} from "react-icons/ai";
-import { memo } from "react"
+import { AiOutlineShoppingCart,AiTwotoneMail ,AiOutlineUser ,AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineTwitter} from "react-icons/ai";
+import { memo, useState } from "react"
 import { Link } from "react-router-dom";
 import { formatter } from "utils/fomater";
+import { ROUTERS } from "utils/router";
 
 const Header = () => {
+
+    const [menus, setMenus] = useState([
+        {
+            name: "Trang chủ",
+            path: ROUTERS.USER.HOME,
+        },
+        {
+            name: "Cửa hàng",
+            path: ROUTERS.USER.PRODUCT,
+
+        },
+        {
+            name: "Sản phẩm",
+            path: "",
+            isShowSubMenu: false,
+            child: [
+                {
+                    name: "Thịt",
+                    path: "",
+                },
+                {
+                    name: "Rau Củ",
+                    path: "",
+                },
+                {
+                    name: "Thức ăn nhanh",
+                    path: "",
+                },
+            ]
+
+        },
+        {
+            name: "Bài viết",
+            path: "",
+
+        },
+        {
+            name: "Liên hệ",
+            path: "",
+
+        }
+    ])
+
     return (
-        <div className="header_top">
-            <div className="container">
-                <div className="row">
-                    <div className="col-6 header_top_left">
-                        <ul>
-                            <li>
-                                <AiTwotoneMail />
-                                bookstore@gmail.com
-                            </li>
-                            <li>Miễn phí giao hàng cho đơn trên {formatter(200000)}</li>
-                        </ul>
-                    </div>
-                    <div className="col-6 header_top_right">
-                        <ul>
-                            <li>
-                                <Link to="https://www.facebook.com/huynhquocnom/" target="_blank">
-                                    <AiOutlineFacebook />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="">
-                                    <AiOutlineInstagram />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="">    
-                                <AiOutlineLinkedin />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="">    
-                                <AiOutlineTwitter />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="">    
-                                <AiOutlineUser />
-                                </Link>
-                                <span> Đăng Nhập </span>
-                            </li>
-                        </ul>
+        <>
+            <div className="header_top">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 header_top_left">
+                            <ul>
+                                <li>
+                                    <AiTwotoneMail />
+                                    bookstore@gmail.com
+                                </li>
+                                <li>Miễn phí giao hàng cho đơn trên {formatter(200000)}</li>
+                            </ul>
+                        </div>
+                        <div className="col-6 header_top_right">
+                            <ul>
+                                <li>
+                                    <Link to="https://www.facebook.com/huynhquocnom/" target="_blank">
+                                        <AiOutlineFacebook />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="">
+                                        <AiOutlineInstagram />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="">    
+                                    <AiOutlineLinkedin />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="">    
+                                    <AiOutlineTwitter />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="">    
+                                    <AiOutlineUser />
+                                    </Link>
+                                    <span> Đăng Nhập </span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-3">
+                        <div className="header_logo" >
+                            <h1>Book Store</h1>
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <nav className="header_menu">
+                            <ul>
+                                {menus?.map((menu, menukey) => (
+                                        <li key ={menukey} className="active">
+                                            <Link to={menu?.path}>{menu?.name}</Link>
+                                        </li>
+                                ))}  
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="col-xl-3">
+                        <div className="header_cart" >
+                            <div className="header_cart_price">
+                                <span>{formatter(1113000)}</span>
+                            </div>
+                            <ul>
+                                <li>
+                                    <Link to="#">
+                                        <AiOutlineShoppingCart />
+                                        <span>5</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- fe/bookstore/src/utils/router.js
+++ fe/bookstore/src/utils/router.js
@@ -2,6 +2,6 @@ export const ROUTERS = {
     USER: {
         HOME: "",
         PROFILE: "profile",
-
+        PRODUCT: "product",
     }
 }
