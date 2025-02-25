9e62ddc2e47254846dc345d0d0da2c50d7841c28
Huynh Nam <namhuynh.101104@gmail.com>
2025-02-25 15:11:57 +0700

    Ngay 25/2

--- fe/bookstore/src/pages/users/theme/header/index.js
+++ fe/bookstore/src/pages/users/theme/header/index.js
@@ -1,7 +1,57 @@
+import "./style.scss";
+import {AiTwotoneMail ,AiOutlineUser ,AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineTwitter} from "react-icons/ai";
 import { memo } from "react"
+import { Link } from "react-router-dom";
+import { formatter } from "utils/fomater";
 
 const Header = () => {
-    return <h1>Header</h1>;
+    return (
+        <div className="header_top">
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 header_top_left">
+                        <ul>
+                            <li>
+                                <AiTwotoneMail />
+                                bookstore@gmail.com
+                            </li>
+                            <li>Miễn phí giao hàng cho đơn trên {formatter(200000)}</li>
+                        </ul>
+                    </div>
+                    <div className="col-6 header_top_right">
+                        <ul>
+                            <li>
+                                <Link to="https://www.facebook.com/huynhquocnom/" target="_blank">
+                                    <AiOutlineFacebook />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="">
+                                    <AiOutlineInstagram />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="">    
+                                <AiOutlineLinkedin />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="">    
+                                <AiOutlineTwitter />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="">    
+                                <AiOutlineUser />
+                                </Link>
+                                <span> Đăng Nhập </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default memo(Header);
