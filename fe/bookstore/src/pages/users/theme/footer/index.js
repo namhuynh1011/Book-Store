1c0e83c6f5c3721f07b576c8d317dd5d1c661e6a
Huynh Nam <namhuynh.101104@gmail.com>
2025-02-26 17:38:42 +0700

    Xong B9

--- fe/bookstore/src/pages/users/theme/footer/index.js
+++ fe/bookstore/src/pages/users/theme/footer/index.js
@@ -7,7 +7,7 @@ const Footer = () => {
     return <footer className="footer">
         <div className="container">
             <div className="row">
-                <div className="col-lg-3">
+                <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                     <div className="footer_about">
                         <h1 className="footer_about_logo">Book Store</h1>
                         <ul>
@@ -17,7 +17,7 @@ const Footer = () => {
                         </ul>
                     </div>
                 </div>
-                <div className="col-lg-6">
+                <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                     <div className="footer_widget">
                         <h6>Cửa Hàng</h6>
                         <ul>
@@ -44,14 +44,14 @@ const Footer = () => {
                         </ul>
                     </div>
                 </div>
-                <div className="col-lg-3">
+                <div className="col-lg-3 col-md-12 col-sm-12 col-xs-12">
                     <div className="footer_widget">
                         <h6>Khuyến mãi & ưu đãi</h6>
                         <p>Đăng ký nhận thông tin tại đây</p>
                         <form action="#">
-                            <div>
+                            <div className="input_group">  
                                 <input type="text" placeholder="Nhập email"/>
-                                <button type="submit" className="butuon_submit">
+                                <button type="submit" className="button_submit">
                                     Đăng ký
                                 </button>
                             </div>
